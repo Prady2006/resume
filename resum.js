818520acbcfb3ad0a5c7681f9fa4969188db31a6
Prady2006 <pradyumnsharma7@gmail.com>
2020-06-02 21:24:50 +0000

    smooth scroll

--- resum.js
+++ resum.js
@@ -1,24 +1,46 @@
 var a = document.querySelectorAll('.nav-menu > li a[href*="#"]');
 
+    // Commented code also gives smooth scroll 
 
-for ( var i =0 ; i < a.length ; i++){
-    a[i].addEventListener('click', function(event){
-        // console.log(event);
+// for ( var i =0 ; i < a.length ; i++){
+//     a[i].addEventListener('click', function(event){
+//         // console.log(event);
+//         event.preventDefault();
+//         // console.log(event.cancelable); // true
+//         var targetSectionId = this.textContent.trim().toLowerCase();
+//         // console.log(targetSectionId);
+//         var targetSection = document.getElementById(targetSectionId);
+//         // console.log(targetSection);
+//         // console.log(ta   rgetSectionCoordinates);
+//         var interval = setInterval(function(){
+//             var targetSectionCoordinates = targetSection.getBoundingClientRect();
+//             // console.log(targetSectionCoordinates);
+//             if(targetSectionCoordinates.top <= 0 ){
+//                 clearInterval(interval);
+//                 return ;
+//             }
+//             window.scrollBy(0,50);
+//         },50);
+//     });
+// }
+var interval = 0 ;
+for (var i = 0 ; i < a.length ; i++){
+    a[i].addEventListener('click',function(event){
         event.preventDefault();
-        // console.log(event.cancelable); // true
         var targetSectionId = this.textContent.trim().toLowerCase();
-        // console.log(targetSectionId);
         var targetSection = document.getElementById(targetSectionId);
-        // console.log(targetSection);
-        // console.log(ta   rgetSectionCoordinates);
-        var interval = setInterval(function(){
-            var targetSectionCoordinates = targetSection.getBoundingClientRect();
-            console.log(targetSectionCoordinates)
-            if(targetSectionCoordinates.top <= 0 ){
-                clearInterval(interval);
-                return ;
-            }
-            window.scrollBy(0,50);
-        },50);
+        interval = setInterval(function(){
+            scrolldown(targetSection);
+        },100);
     });
+}
+function scrolldown(targetSection){
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
+    // console.log(targetSectionCoordinates);   
+    if(targetSectionCoordinates.top <= 0 ){
+        clearInterval(interval);
+        return;
+    }
+    window.scrollBy(0,50);
+    console.log(targetSectionCoordinates.top);
 }
